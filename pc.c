5705713f16830a97b14458aed9bc2de8db5b2f92
llullanqui <luis.1142@hotmail.com>
2017-08-10 16:13:12 -0500

    A punto de probar

--- pc.c
+++ pc.c
@@ -14,27 +14,46 @@ pthread_cond_t cons;
 pthread_mutex_t lock;
 int total;
 int tam_cola;
+int cola;
+float tiempo_pro;
+float tiempo_con;
 
 void *productor(void *estructura);
 void *consumidor(void *estructura);
 
 typedef struct mi_estructuraTDA{
-	int cola;
-	float tiempo;
+	int id;	
 }estructura;
 
-void *productor(void *estructura){
-	estructura *datos = (estructura *) estructura;
-	int cola = i->cola;
-	
-	while(cola>=){
-
+void *productor(void *arg){
+	estructura *datos = (estructura *) arg;
+	int id = datos->id;
+	pthread_mutex_lock(&lock);
+	while(cola>=tam_cola){
+		pthread_cond_wait(&prod,&lock);
 	}
+	usleep(tiempo_pro*1000000);
+	cola++;
+	printf("Productor %i ha producido 1 item, tamaño de cola = %i",id,(tam_cola-cola));
+	pthread_mutex_unlock(&lock);
+	pthread_cond_broadcast(&cons);
+	
 	return (void *)0;
 }
 
-void *consumidor(void *estructura){
-
+void *consumidor(void *arg){
+	estructura *datos = (estructura *) arg;
+	int id = datos->id;
+	pthread_mutex_lock(&lock);
+	while((cola<=0)){
+		pthread_cond_wait(&cons,&lock);
+	}
+	usleep(tiempo_cons*1000000);
+	cola--;
+	printf("Consumidor %i ha consumido 1 item, tamaño de cola = %i",id,(tam_cola-cola));
+	pthread_mutex_unlock(&lock);
+	pthread_cond_broadcast(&prod);
+	
 	return (void *)0;
 }
 
@@ -51,15 +70,15 @@ int main(int argc, char *argv[]){
     }
 
 	int num_hilos_p = atoi(argv[1]);
-	float tiempo_pro = atoi(argv[2]);
-	float tiempo_con = atoi(argv[4]);
+	tiempo_pro = atoi(argv[2]);
+	tiempo_con = atoi(argv[4]);
 	int num_hilos_c = atoi(argv[3]);
-	int tam_cola = atoi(argv[5]);
+	tam_cola = atoi(argv[5]);
 	total = atoi(argv[6]);
 	prod = PTHREAD_COND_INITIALIZER;
 	cons = PTHREAD_COND_INITIALIZER;
 
-	int cola = 0;
+	cola = 0;
 
 	pthread_t *productores;
 	pthread_t *consumidores;
@@ -68,7 +87,15 @@ int main(int argc, char *argv[]){
 
 	int i;
 	for(i=0;i<num_hilos_p;i++){
+		estructura *estruc = (estructura *)malloc(sizeof(estructura *));
+		estruc->id = i+1;
+		int hilo = pthread_create(&(productor+i), NULL, productores,(void *)estruc);
+	}
 	
+	for(i=0;i<num_hilos_p;i++){
+		estructura *estruc = (estructura *)malloc(sizeof(estructura *));
+		estruc->id = i+1;
+		int hilo = pthread_create(&(consumidor+i), NULL, consumidores,(void *)estruc);
 	}
 
 	return 0;
