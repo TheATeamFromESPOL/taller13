71cf238af86e7cbe3f06ca68f5571b4dcbef8067
llullanqui <luis.1142@hotmail.com>
2017-08-10 15:31:55 -0500

    pdf

--- pc.c
+++ pc.c
@@ -4,40 +4,54 @@
 #include <pthread.h>
 #include <unistd.h>
 
+//Para inicializar una variable de condicion
+//pthread_cond_t cu = PTHREAD_COND_INITIALIZER
+//Usar usleep
+//hacer dos variables condicion y un mutex
 
-pthread_mutex_t lock;
-
-void *funcion_hilo(void *estruc);
-
+void *productor(void *estructura);
+void *consumidor(void *estructura);
 
 typedef struct mi_estructuraTDA{
-	int linea_inicio;
-	int linea_final;
-	int *tam_lineas;
-	char *ruta;
+	int cola;
+	pthread_mutex_t lock;
 }estructura;
 
+void *productor(void *estructura){
+	estructura *datos = (estructura *) estructura;
+	int cola = i->cola;
+	
 
-void * funcion_hilo(void *estruc){
+	while(1){
+		
+	}
+	return (void *)0;
+}
 
+void *consumidor(void *estructura){
 
 	return (void *)0;
 }
 
 int main(int argc, char *argv[]){
 
-	if(argc<7){
+	if(argc!=7){
 		printf("Cantidad de argumentos incorrecta");
 		return -1;
 	}
-	else{
-		int cont=0;	
-		if (pthread_mutex_init(&lock, NULL) != 0){
-		    	printf("\n mutex init failed\n");
-		    	return 1;
-			}
-
+	int num_hilos_p = atoi(argv[1]);
+	int tiempo_pro = atoi(argv[2]);
+	int tiempo_con = atoi(argv[4]);
+	int num_hilos_c = atoi(argv[3]);
+	int tam_cola = atoi(argv[5]);
+	int total = atoi(argv[6]);
+	int cola = 0;
+	
+	if (pthread_mutex_init(&lock, NULL) != 0){
+	    printf("\n mutex init failed\n");
+		return 1;
 	}
+
 
 	return 0;
 }
